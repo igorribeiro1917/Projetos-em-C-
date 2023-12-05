dc03daf4accd9b93dc599af892435230d6531cb5
Igor Ribeiro <igorribeiroifrj@gmail.com>
2023-12-05 16:28:14 -0300

    Gestor de Estoque orientado ao objeto

---
+++ Gestor de Estoque(Projeto#3)/IEstoque.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_Estoque_Projeto_3_
+{
+    internal interface IEstoque
+    {
+        void AdicionarEntrada();
+        void AdicionarSaida();
+        void Exibir();
+    
+      
+    
+    
+    
+    }
+}
